c0f5035104d4528ee0e2d3f513f14f51729b4aea
mkandanmk7 <mkandanmk7@gmail.com>
2021-12-23 11:06:25 +0530

    useReducer

--- src/Components/UseReducerHooks.js
+++ src/Components/UseReducerHooks.js
@@ -1,13 +1,13 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer } from "react";
 const initialState = 0;
 const CounterReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "addcount": {
+    case "addcount":
       return state + 1;
-    }
-    case "deccount": {
+
+    case "deccount":
       return state - 1;
-    }
+
     default:
       return state;
   }
@@ -17,14 +17,14 @@ const UseReducerHooks = () => {
   //   const [counter, setCounter] = useState(0);
   //state used for get state value from reducer function;
   const [state, dispatch] = useReducer(CounterReducer, initialState);
-  //   console.log(state);
+  console.log(state);
 
   // counter example
   return (
     <div>
       <h2>Counter Value is {state}</h2>
-      <button onClick={dispatch({ type: "addcount" })}>Inc</button>
-      <button onClick={dispatch({ type: "deccount" })}>Dec</button>
+      <button onClick={() => dispatch({ type: "addcount" })}>Inc</button>
+      <button onClick={() => dispatch({ type: "deccount" })}>Dec</button>
     </div>
   );
 };
